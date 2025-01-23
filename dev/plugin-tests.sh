63dc0bf5f738cec175b48ea5d82de13e720c88c6
Rick Newton-Rogers <rnro@apple.com>
2025-01-23 09:22:39 +0000

    plugin-tests.sh handles being sourced

--- dev/plugin-tests.sh
+++ dev/plugin-tests.sh
@@ -19,8 +19,15 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [[ -n ${GITHUB_ACTIONS:=""} ]]; then
+    # we will have been piped to bash and won't know the location of the script
+    echo "Running in GitHub Actions"
+    source_directory="$(pwd)"
+else
+    here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    source_directory="$(readlink -f "${here}/..")"
+fi
 tests_directory="${PLUGIN_TESTS_OUTPUT_DIRECTORY:=$(mktemp -d)}"
 
-PLUGIN_TESTS_OUTPUT_DIRECTORY="$tests_directory" "$here/setup-plugin-tests.sh"
-PLUGIN_TESTS_DIRECTORY="$tests_directory" "$here/execute-plugin-tests.sh"
+PLUGIN_TESTS_OUTPUT_DIRECTORY="$tests_directory" "${source_directory}/dev/setup-plugin-tests.sh"
+PLUGIN_TESTS_DIRECTORY="$tests_directory" "${source_directory}/dev/execute-plugin-tests.sh"
